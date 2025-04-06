c09b5b562595acf7a4a17db94ca7ad8121095558
elgezett <75252196+elgezett@users.noreply.github.com>
2025-04-06 18:34:13 +0200

    Add files via upload
    new content

---
+++ script.js
@@ -0,0 +1,15 @@
+VANTA.BIRDS({
+    el: "#bird-effect",
+    backgroundAlpha: 0.0,
+    color1: 0x1f2f46, // Primary
+    color2: 0xFFEB35, // Accent
+    birdSize: 1.4,
+    speedLimit: 5.0,
+    separation: 20,
+    alignment: 20,
+    cohesion: 55,
+    quantity: 4,
+    mouseControls: true,
+    touchControls: true,
+  });
+  
